cfd767ff77ca32419da347f4c17d6cdbd9cc32a2
tâm nguyễn <tamcb2708@gmail.com>
2021-02-28 15:39:46 +0700

    sua truc tiep

---
+++ Commom/MailHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Configuration;
+using System.Net;
+namespace Commom
+{
+    public class MailHelper
+    {
+        public void SendMail(string toEmailAddress,string subject,string content)
+        {
+            var fromEmailAddress = ConfigurationManager.AppSettings["FromEmailAddress"].ToString();
+            var fromEmailDisplayName = ConfigurationManager.AppSettings["FromEmailDisPlayName"].ToString();
+            var fromEmailPassword = ConfigurationManager.AppSettings["FromEmailPassWord"].ToString();
+            var smtpHost = ConfigurationManager.AppSettings["SMTPHost"].ToString();
+            var smtpPort = ConfigurationManager.AppSettings["SMTPPort"].ToString();
+
+            bool enableSsl = bool.Parse(ConfigurationManager.AppSettings["EnableSSL"].ToString());
+
+            string body = content;
+            MailMessage message = new MailMessage(new MailAddress(fromEmailAddress, fromEmailDisplayName), new MailAddress(toEmailAddress));
+            message.Subject = subject;
+            message.IsBodyHtml = true;
+            message.Body = body;
+
+            var client = new SmtpClient();
+            client.Credentials = new NetworkCredential(fromEmailAddress, fromEmailPassword);
+            client.Host = smtpHost;
+            client.EnableSsl = enableSsl;
+            client.Port = !string.IsNullOrEmpty(smtpPort) ? Convert.ToInt32(smtpPort) : 0;
+            client.Send(message);
+
+        }
+    }
+}
